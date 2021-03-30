63f904b9499debc2b32d9149eb0eac790e9840f1
peremanresa <pere.manresa@bcgdv.com>
2021-03-30 14:32:44 +0200

    fixes

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,9 @@
-import * as React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import * as React from 'react';
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+  <div>
+    <h1>Page not found</h1>
+  </div>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
